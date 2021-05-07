d629c1997e63819b2aee9b3ae2f2ceab902807b0
tsdavid <ipackcorpdavid@gmail.com>
2021-05-07 18:22:45 +0900

    3.4 Make API CRUD

---
+++ src/main/java/com/dk/springboot/book/service/posts/PostsService.java
@@ -0,0 +1,44 @@
+package com.dk.springboot.book.service.posts;
+
+import com.dk.springboot.book.domain.posts.Posts;
+import com.dk.springboot.book.domain.posts.PostsRepository;
+import com.dk.springboot.book.web.dto.PostsResponseDto;
+import com.dk.springboot.book.web.dto.PostsSaveRequestDto;
+import com.dk.springboot.book.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return this.postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id)
+        );
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+
+    public PostsResponseDto findById(Long id) {
+
+        Posts entity = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id)
+        );
+
+        return new PostsResponseDto(entity);
+    }
+}
